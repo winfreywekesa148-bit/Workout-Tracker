6062d18ceb6a5552e7ccc6a712b3028a499af6bf
Winkesa <winfreywekesa148@gmail.com>
2026-08-01 15:38:20 +0300

    Add Exercise model with database columns

--- models/Exercise.py
+++ models/Exercise.py
@@ -1,1 +1,13 @@
+from extensions import db
 
+class Exercise(db.Model):
+
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Interger, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=True)
+
+    # relationships
+    workoutsexercises = db.relationship('Workout', back_populates='exercises')
