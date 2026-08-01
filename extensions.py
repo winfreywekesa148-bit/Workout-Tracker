2007406336fa547bf22d0274206fb8a53ca01a8e
Winkesa <winfreywekesa148@gmail.com>
2026-08-01 17:40:46 +0300

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_marshmallow import Marshmallow
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-jwt = JWTManager()
-ma = Marshmallow()
-migrate = Migrate()
