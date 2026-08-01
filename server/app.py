36a330fd33f2e13961cdd3d734c1733840bd29ee
Winkesa <winfreywekesa148@gmail.com>
2026-08-01 17:11:20 +0300

    Expand API with exercise and workout exercise routes
    Added endpoints for managing exercises and workout exercises.

--- server/app.py
+++ server/app.py
@@ -43,8 +43,49 @@ def delete_workout(id):
     db.session.commit()
     return '', 204
 
+# get all exercises
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+    exercises = Exercise.query.all()
+    return jsonify([exercise.to_dict() for exercise in exercises])
 
+# get each exercise
+@app.route('/exercises/<int:id>', methods=['GET'])
+def get_exercise(id):
+    exercise = Exercise.query.get_or_404(id)
+    return jsonify(exercise.to_dict())
 
+# create a new exercise
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    data = request.get_json()
+    exercise = Exercise(name=data['name'], category=data['category'], equipment_needed=data('equipment_needed', False))
+    db.session.add(exercise)
+    db.session.commit()
+    return jsonify(exercise.to_dict()), 201
+
+# delete an exercise
+@app.route('/exercises/<int:id>', methods=['DELETE'])
+def delete_exercise(id):
+    exercise = Exercise.query.get_or_404(id)
+    db.session.delete(exercise)
+    db.session.commit()
+    return jsonify({"message": f"Exercise {id} deleted"})
+
+# get all workout exercises
+@app.route('/workout_exercises', methods=['GET'])
+def get_workout_exercises():
+    workout_exercises = WorkoutExercise.query.all()
+    return jsonify([we.to_dict() for we in workout_exercises])
+
+# create a new workout exercise
+@app.route('/workout_exercises', methods=['POST'])
+def create_workout_exercise():
+    data = request.get_json()
+    workout_exercise = WorkoutExercise(workout_id=data['workout_id'], exercise_id=data['exercise_id'], sets=data['sets'], reps=data['reps'], duration_seconds=data('duration_seconds'))
+    db.session.add(workout_exercise)
+    db.session.commit()
+    return jsonify(workout_exercise.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
